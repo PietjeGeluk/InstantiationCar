db973bd7afe1fc6b94140faf0aa3fc610514c508
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-19 08:05:30 +0100

    Add files via upload
    ASP form application with instantiation.

---
+++ Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8
+{
+    public class Car
+    {
+        public string brand;
+        public string model;
+        public int year;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication8
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void btnOk_Click(object sender, EventArgs e)
+        {
+            Car x = new Car();
+            x.brand = "BMW";
+            x.model = "316";
+            x.year = 2014;
+
+            Label1.Text = x.brand;
+        }
+    }
+}
